216264f8397f7a6987aafdcad0241b4ced81cdec
Brendon dos Reis Carvalho <contato@brendon.com.br>
2021-07-25 09:39:31 -0300

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+const repository = {
+  name: 'unform',
+  description: 'A simple, lightweight and flexible React.js component for managing forms',
+  link: 'https://github.com/unform'
+}
+
+export function RepositoryList() {
+  return(
+    <section>
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        
+      </ul>
+
+    </section>
+  )
+};
